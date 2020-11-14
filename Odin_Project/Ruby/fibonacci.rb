76c42cd6a43a27bff87ad3dd240325f325f7c645
Ara <hello.ara@pm.me>
2020-11-14 23:10:56 +0900

    fibonacci done

--- Odin_Project/Ruby/fibonacci.rb
+++ Odin_Project/Ruby/fibonacci.rb
@@ -21,21 +21,7 @@ def fibs_rec(n)
   elsif n == 2
     [0, 1]
   else
-    seq = fibs_rec(n-1)
-    seq << seq[-2] + seq[-1]
+    array = fibs_rec(n-1)
+    array << seq[-2] + seq[-1]
   end
 end
-
-def merge_sort(array)
-  n = array.size
-  if n < 2
-    array
-  else
-    i = n / 2
-    merge_sort(array[0..i]) + merge_sort(array[i+1..-1])
-    n -= 2
-  end
-end
-
-array = [9,3,2,11,8,3,4,5]
-puts merge_sort(array)
